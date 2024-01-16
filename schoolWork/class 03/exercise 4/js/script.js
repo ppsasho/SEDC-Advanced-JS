16a29cc828a18e59faf11a18d227631cc1c64695
ppsasho <businessmail5545@gmail.com>
2024-01-16 17:30:22 +0100

    - schoolwork

--- schoolWork/class 03/exercise 4/js/script.js
+++ schoolWork/class 03/exercise 4/js/script.js
@@ -1,5 +1,4 @@
-let students = [];
-let currentYear = 2024;
+  let currentYear = 2024;
 
 function createStudents(firstName, lastName, birthYear, academy, grades) {
   (this.firstName = firstName),
@@ -8,7 +7,7 @@ function createStudents(firstName, lastName, birthYear, academy, grades) {
     (this.academy = academy),
     (this.grades = grades),
     (this.getAge = function () {
-      return `The student is ${currentYear - this.birthYear} years old`;
+      return `The student is ${(currentYear - this.birthYear)} years old`;
     });
   this.getInfo = function () {
     return `This is a student ${this.firstName} ${this.lastName} from ${this.academy}!`;
@@ -23,4 +22,26 @@ function createStudents(firstName, lastName, birthYear, academy, grades) {
   };
 }
 
-for (let i = 0; i < 3; i++) {}
+function create3Students() {
+  let students = [];
+  for (let i = 0; i < 3; i++) {
+    let firstName = prompt('Enter your first name');
+    let lastName = prompt('Enter your last name');
+    let birthYear = prompt('Enter your birth year');
+    let academy = prompt('Enter your academy');
+    let grade1 = prompt('Enter your first grade');
+    let grade2 = prompt('Enter your second grade');
+    let grade3 = prompt('Enter your third grade');
+    let grade4 = prompt('Enter your fourth grade');
+    let grade5 = prompt('Enter your fifth grade');
+    let grades = [grade1, grade2, grade3, grade4, grade5];
+    let student = createStudents(firstName, lastName, birthYear, academy, grades);
+    students.push(student);
+    console.log(student);
+  }
+  return 'students created successfully!';
+}
+
+let btn = document.querySelector('button').addEventListener('click', function() {
+  create3Students();
+})
