9514faa9c64473adcb705c1ec9f7fa0af3117366
ppsasho <businessmail5545@gmail.com>
2024-02-10 21:44:43 +0100

    - beer update

--- homeWork/Beer bar/src/js/script.js
+++ homeWork/Beer bar/src/js/script.js
@@ -2,8 +2,26 @@ let content = document.getElementById('content');
 let beers = document.getElementById('beers');
 let randomBeer = document.getElementById('random-beer');
 let spinner = document.getElementById('spinner');
+let pageNav = document.getElementById('pageNav');
+let nextBtn = document.getElementById('nextBtn');
+let prevBtn = document.getElementById('prevBtn');
+let sortName = document.getElementById('filterName');
+let sortAlc= document.getElementById('filterAlc');
+let sortBrewed= document.getElementById('filterBrewed');
+let sortIbu= document.getElementById('filterIbu');
+let sort5 = document.getElementById('filter5');
+let sort10 = document.getElementById('filter10');
+let sort20 = document.getElementById('filter20');
+let filterNav = document.getElementsByClassName('filterNav')[0];
 
+filterNav.style.visibility = 'hidden';
+pageNav.style.visibility = 'hidden';
 spinner.style.visibility = 'hidden';
+let currentPage = 0;
+let perPage = null;
+let filter = null;
+let currentBeers = null;
+
 
 async function getBeerData (url) {
     spinner.style.visibility = 'visible';
@@ -12,6 +30,7 @@ async function getBeerData (url) {
     .then(data => {
         console.log(data);
         displayBeer(createBeer(data));
+        currentBeers = data;
         spinner.style.visibility = 'hidden';
     })
 }
@@ -22,7 +41,7 @@ let displayBeer = array => {
     content.innerHTML = '';
     for(let i = 0; i < array.length; i++) {
         let cardBox = document.createElement('div');
-        cardBox.classList.add('card', 'border-dark', 'text-bg-dark', 'h-100', 'text-center');
+        cardBox.classList.add('card', 'border-secondary', 'text-bg-dark', 'h-100', 'text-center');
         content.appendChild(cardBox);
         cardBox.style.width = '18rem';
         cardBox.style.padding = '20px';
@@ -45,7 +64,7 @@ let displayBeer = array => {
         cardBtn.classList.add('btn', 'btn-primary');
         cardBtn.addEventListener('click', () => {
             console.log('Card infoBtn clicked');
-            moreBeerInfo({name, image_url, description, tagline, first_brewed, abv, ibu, food_pairing} = array[i]);
+            moreBeerInfo(array[i]);
         })
         cardBtn.innerText = 'More details';
         let cardFooter = document.createElement('div');
@@ -58,6 +77,8 @@ let displayBeer = array => {
         cardBox.appendChild(cardBody);
         console.log(cardBox);
     }
+    pageNav.style.visibility = 'visible';
+    filterNav.style.visibility = 'visible';
 }
 
 function Beer (object) {
@@ -71,79 +92,103 @@ function Beer (object) {
     this.foodPairing = object.food_pairing
 }
 
-/*<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div> */
 
-let moreBeerInfo = (name, img, desc, tag, brewed, abv, ibu, food) => {
+
+let moreBeerInfo = (object) => {
     content.innerHTML = '';
-    // console.log(name, img, desc, tag, brewed, abv, ibu, food);
+    console.log(object);
+    const {name, img, description, tagline, brewed, abv, ibu, foodPairing} = object;
+    console.log('Preparing beer info...');
+
     let cardBox = document.createElement('div');
-    cardBox.classList.add('card', 'mb-3');
-    cardBox.style.maxWidth = '20rem';
+    cardBox.classList.add('card', 'mb-3', 'text-bg-dark', 'border-warning');
+    cardBox.style.width = '80rem';
+    cardBox.style.maxHeight = '50rem';
 
     let cardRow = document.createElement('div');
     cardRow.classList.add('row', 'g-0');
 
     let imgFrame = document.createElement('div');
-    imgFrame.classList.add('col-md-4');
+    imgFrame.classList.add('col-md-4', 'dark');
+    imgFrame.style.padding = '3rem'
 
     let imgShow = document.createElement('img');
     imgShow.src = img;
+    imgShow.style.maxHeight = '30rem';
     imgShow.classList.add('img-fluid', 'rounded-start');
     imgFrame.appendChild(imgShow);
-    imgFrame.appendChild(cardRow);
+    cardRow.appendChild(imgFrame);
 
-    let cardBodyFrame = document.createElement('div'); // Inside cardRow
+    let cardBodyFrame = document.createElement('div');
+    cardBodyFrame.style.padding = '1rem'
     cardBodyFrame.classList.add('col-md-8');
 
-    let cardBody = document.createElement('div'); // Inside cardBodyFrame
+    let cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
+    
+    let cardTitle = document.createElement('h4');
+    cardTitle.style.display = 'inline';
+    cardTitle.innerText = `${name} `;
 
-    let cardHeader = document.createElement('div'); // Inside cardBody
+    let cardHeader = document.createElement('div');
+
     cardHeader.style.display ='inline';
-    cardHeader.classList.add('card-header', 'card-title');
-    cardHeader.innerText = name;
+    cardHeader.classList.add('card-header');
 
-    let cardTag = document.createElement('span'); // Inside cardBody
-    cardTag.innerText = tag;
+    let cardTag = document.createElement('span');
+    cardTag.style.marginLeft = '1rem';
+    cardTag.innerText = ` ${tagline}`;
 
+    cardHeader.appendChild(cardTitle);
     cardHeader.appendChild(cardTag);
 
-    let cardText = document.createElement('p'); // Inside cardBody
-    cardText.innerText = desc;
+    let cardText = document.createElement('p');
+    cardText.style.marginTop = '2rem'
+    cardText.style.textAlign = 'justify'
+    cardText.innerText = description;
 
-    let cardBrewed = document.createElement('p'); // Inside cardBody
+    let cardBrewed = document.createElement('p');
+    cardBrewed.style.marginTop = '1.5rem'
     cardBrewed.innerText = `Brewed: ${brewed}`;
 
-    let cardAbv = document.createElement('p'); // Inside cardBody
-    cardAbv.innerText = `alcohol: ${abv}`;
+    let cardAbv = document.createElement('p'); 
+    cardAbv.innerText = `alcohol: ${abv}%`;
 
-    let cardIbu = document.createElement('p'); // Inside cardBody
-    cardIbu.innerText = `Bitterness: ${ibu}`;
+    let cardIbu = document.createElement('p'); 
+    cardIbu.innerText = `Bitterness: ${ibu} IBU`;
 
-    let cardList = document.createElement('ul'); // Inside cardBody
+    let foodCombo = document.createElement('h5');
+    foodCombo.style.marginTop = '2rem'
+    foodCombo.style.fontWeight = 'bold'
+    foodCombo.innerText = 'Food Pairing';
+
+    let cardList = document.createElement('ul');
     cardList.classList.add('list-group', 'list-group-flush');
 
 
-    for(let i = 0; i < food.length; i++) {
+    for(let i = 0; i < foodPairing.length; i++) {
         let pair = document.createElement('li');
         pair.classList.add('list-group-item');
+        pair.innerText = foodPairing[i];
         cardList.appendChild(pair);
     }
 
+    cardBody.appendChild(cardHeader);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(cardBrewed);
+    cardBody.appendChild(cardAbv);
+    cardBody.appendChild(cardIbu);
+    cardBody.appendChild(foodCombo);
+    cardBody.appendChild(cardList);
+    cardBodyFrame.appendChild(cardBody);
+    cardRow.appendChild(cardBodyFrame);
+    cardBox.appendChild(cardRow);
+    content.appendChild(cardBox);
+
+    pageNav.style.visibility = 'hidden';
+    filterNav.style.visibility = 'hidden';
 }
+
 
 let createBeer = array => {
     let data = [...array];
@@ -155,14 +200,77 @@ let createBeer = array => {
     return beers;
 }
 
-randomBeer.addEventListener('click', () => {
+
+let getRandomBeer = () => {
     console.clear();
     console.log('Random beer clicked');
     getBeerData('https://api.punkapi.com/v2/beers/random');
-})
+    filterNav.style.visibility = 'hidden';
+}
 
-beers.addEventListener('click', () => {
+let getBeers = () => {
     console.clear();
     console.log('Beers clicked');
-    getBeerData('https://api.punkapi.com/v2/beers');
-})
+    getBeerData('https://api.punkapi.com/v2/beers?page=1&per_page=25');
+    // 'https://api.punkapi.com/v2/beers/beer_name=punk'
+}
+
+let sortPerPage = pageNum => {
+
+}
+
+let pageFilterBy = filter => {
+
+}
+
+// for name searching('https://api.punkapi.com/v2/beers?beer_name=${input}&per_page=${amount} of results in page}')
+
+randomBeer.addEventListener('click', getRandomBeer);
+
+beers.addEventListener('click', getBeers)
+
+let sendNext = () => {
+
+}
+
+let sendPrev = () => {
+    
+}
+
+let sortedNames = () => {
+    
+}
+
+let sortedAlc = () => {
+
+}
+
+let sortedBrewed = () => {
+
+}
+
+let sortedIbu = () => {
+
+}
+
+let sorted5 = () => {
+
+}
+
+let sorted10 = () => {
+
+}
+
+let sorted20 = () => {
+
+}
+
+nextBtn.addEventListener('click', sendNext)
+prevBtn.addEventListener('click', sendPrev)
+sortName.addEventListener('click', sortedNames)
+sortAlc.addEventListener('click', sortedAlc)
+sortBrewed.addEventListener('click', sortedBrewed)
+sortIbu.addEventListener('click', sortedIbu)
+sort5.addEventListener('click', sorted5)
+sort10.addEventListener('click', sorted10)
+sort20.addEventListener('click', sorted20)
