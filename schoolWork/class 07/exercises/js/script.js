ac502c84b05074546a2a3ecae8fca83cb001aee1
ppsasho <businessmail5545@gmail.com>
2024-02-22 21:07:41 +0100

    - schoolwork class 7 update

--- schoolWork/class 07/exercises/js/script.js
+++ schoolWork/class 07/exercises/js/script.js
@@ -44,6 +44,21 @@ let getAvarageStuff = students => {
         student.age >=20 && student.age <= 30);
     let femArray2 = allFemMails.map(student => student.email).sort((mail1, mail2) => mail1 - mail2);
     console.log(femArray2);
+
+    let studentFullNames = copyObject.filter(student => student.age > 40)
+    .map(student => full = `${student.firstName} ${student.lastName}`).sort((first, second) => second - first);
+    console.log(studentFullNames);
+
+    let countGmail = copyObject.filter(student => student.email.includes('google'));
+    console.log(countGmail.length);
+
+    let avgFem = copyObject.filter(student => 
+        student.gender === 'Female' &&
+        student.city === 'Skopje'
+    ).map(student => student.age);
+    let avgFemAge = avgFem.reduce((acc, curr) => acc + curr, 0);
+    avgFemAge /= avgFem.length;
+    console.log(Math.round(avgFemAge));
 }
 
 getData('https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json');
